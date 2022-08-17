d0583906334704a273689884977d597df18d8f96
tanshaobo <18235140339@163.com>
2022-08-18 01:57:24 +0800

    ci(deps): 修改git cz命令

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import router from '@/router'
 import App from './App.vue'
 import store from './store'
 import './style/common.css'
-import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
