840a55fde346984e8ccfa27ee6de0f6174e14121
tanshaobo <18235140339@163.com>
2022-08-19 23:17:19 +0800

    feat(other): 添加角色天赋配置

--- src/config/role.js
+++ src/config/role.js
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2022-08-19 22:45:05
  * @LastEditors: tanshaobo
- * @LastEditTime: 2022-08-19 22:46:37
+ * @LastEditTime: 2022-08-19 23:16:28
  * @Description: 角色配置
  * @FilePath: \yuanshen-utils\src\config\role.js
  */
@@ -15,203 +15,253 @@ export const roleList = [
   },
   {
     id: '002',
-    Name: '荧'
+    Name: '荧',
+    Talent: []
   },
   {
     id: '003',
-    Name: '安柏'
+    Name: '安柏',
+    Talent: 1
   },
   {
     id: '004',
-    Name: '丽莎'
+    Name: '丽莎',
+    Talent: 3
   },
   {
     id: '005',
-    Name: '凯亚'
+    Name: '凯亚',
+    Talent: 3
   },
   {
     id: '006',
-    Name: '诺艾尔'
+    Name: '诺艾尔',
+    Talent: 2
   },
   {
     id: '007',
-    Name: '香菱'
+    Name: '香菱',
+    Talent: 5
   },
   {
     id: '008',
-    Name: '芭芭拉'
+    Name: '芭芭拉',
+    Talent: 1
   },
   {
     id: '009',
-    Name: '行秋'
+    Name: '行秋',
+    Talent: 6
   },
   {
     id: '010',
-    Name: '砂糖'
+    Name: '砂糖',
+    Talent: 1
   },
   {
     id: '011',
-    Name: '北斗'
+    Name: '北斗',
+    Talent: 6
   },
   {
     id: '012',
-    Name: '雷泽'
+    Name: '雷泽',
+    Talent: 2
   },
   {
     id: '013',
-    Name: '班尼特'
+    Name: '班尼特',
+    Talent: 2
   },
   {
     id: '014',
-    Name: '凝光'
+    Name: '凝光',
+    Talent: 4
   },
   {
     id: '015',
-    Name: '重云'
+    Name: '重云',
+    Talent: 5
   },
   {
     id: '016',
-    Name: '菲谢尔'
+    Name: '菲谢尔',
+    Talent: 3
   },
   {
     id: '017',
-    Name: '七七'
+    Name: '七七',
+    Talent: 4
   },
   {
     id: '018',
-    Name: '琴'
+    Name: '琴',
+    Talent: 2
   },
   {
     id: '019',
-    Name: '迪卢克'
+    Name: '迪卢克',
+    Talent: 2
   },
   {
     id: '020',
-    Name: '刻晴'
+    Name: '刻晴',
+    Talent: 4
   },
   {
     id: '021',
-    Name: '莫娜'
+    Name: '莫娜',
+    Talent: 2
   },
   {
     id: '022',
-    Name: '温迪'
+    Name: '温迪',
+    Talent: 3
   },
   {
     id: '023',
-    Name: '可莉'
+    Name: '可莉',
+    Talent: 1
   },
   {
     id: '024',
-    Name: '迪奥娜'
+    Name: '迪奥娜',
+    Talent: 1
   },
   {
     id: '025',
-    Name: '达达利亚'
+    Name: '达达利亚',
+    Talent: 1
   },
   {
     id: '026',
-    Name: '辛焱'
+    Name: '辛焱',
+    Talent: 6
   },
   {
     id: '027',
-    Name: '钟离'
+    Name: '钟离',
+    Talent: 6
   },
   {
     id: '028',
-    Name: '阿贝多'
+    Name: '阿贝多',
+    Talent: 3
   },
   {
     id: '029',
-    Name: '甘雨'
+    Name: '甘雨',
+    Talent: 5
   },
   {
     id: '030',
-    Name: '魈'
+    Name: '魈',
+    Talent: 4
   },
   {
     id: '031',
-    Name: '胡桃'
+    Name: '胡桃',
+    Talent: 5
   },
   {
     id: '032',
-    Name: '罗莎莉亚'
+    Name: '罗莎莉亚',
+    Talent: 3
   },
   {
     id: '033',
-    Name: '烟绯'
+    Name: '烟绯',
+    Talent: 6
   },
   {
     id: '034',
-    Name: '优菈'
+    Name: '优菈',
+    Talent: 2
   },
   {
     id: '035',
-    Name: '枫原万叶'
+    Name: '枫原万叶',
+    Talent: 5
   },
   {
     id: '036',
-    Name: '神里绫华'
+    Name: '神里绫华',
+    Talent: 8
   },
   {
     id: '037',
-    Name: '宵宫'
+    Name: '宵宫',
+    Talent: 7
   },
   {
     id: '038',
-    Name: '早柚'
+    Name: '早柚',
+    Talent: 9
   },
   {
     id: '039',
-    Name: '雷电将军'
+    Name: '雷电将军',
+    Talent: 9
   },
   {
     id: '040',
-    Name: '九条裟罗'
+    Name: '九条裟罗',
+    Talent: 8
   },
   {
     id: '041',
-    Name: '珊瑚宫心海'
+    Name: '珊瑚宫心海',
+    Talent: 7
   },
   {
     id: '042',
-    Name: '托马'
+    Name: '托马',
+    Talent: 7
   },
   {
     id: '043',
-    Name: '荒泷一斗'
+    Name: '荒泷一斗',
+    Talent: 8
   },
   {
     id: '044',
-    Name: '五郎'
+    Name: '五郎',
+    Talent: 9
   },
   {
     id: '045',
-    Name: '申鹤'
+    Name: '申鹤',
+    Talent: 4
   },
   {
     id: '046',
-    Name: '云堇'
+    Name: '云堇',
+    Talent: 5
   },
   {
     id: '047',
-    Name: '八重神子'
+    Name: '八重神子',
+    Talent: 9
   },
   {
     id: '048',
-    Name: '神里绫人'
+    Name: '神里绫人',
+    Talent: 8
   },
   {
     id: '049',
-    Name: '夜兰'
+    Name: '夜兰',
+    Talent: 4
   },
   {
     id: '050',
-    Name: '久岐忍'
+    Name: '久岐忍',
+    Talent: 8
   },
   {
     id: '051',
-    Name: '鹿野院平藏'
+    Name: '鹿野院平藏',
+    Talent: 7
   }
 ]
 
