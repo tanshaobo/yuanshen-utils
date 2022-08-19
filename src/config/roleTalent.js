541d838b6021add9a27814326239cef33a5a11a4
tanshaobo <18235140339@163.com>
2022-08-19 23:20:02 +0800

    fix(other): 修改天赋材料周配置

--- src/config/roleTalent.js
+++ src/config/roleTalent.js
@@ -2,11 +2,11 @@
  * @Author: tanshaobo
  * @Date: 2022-08-18 02:30:08
  * @LastEditors: tanshaobo
- * @LastEditTime: 2022-08-19 23:17:53
+ * @LastEditTime: 2022-08-19 23:19:32
  * @Description: 角色天赋
  * @FilePath: \yuanshen-utils\src\config\roleTalent.js
  */
-export const weekData = {
+export const weekType = {
   1: [0, 1, 4],
   2: [0, 2, 5],
   3: [0, 3, 6]
