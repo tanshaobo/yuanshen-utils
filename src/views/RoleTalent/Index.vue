f8be29cbfefc459ddde0a753d0cbf97210ec88b3
tanshaobo <18235140339@163.com>
2022-08-19 22:44:15 +0800

    feat(components): 添加静态页

--- src/views/RoleTalent/Index.vue
+++ src/views/RoleTalent/Index.vue
@@ -1,7 +1,51 @@
 <template>
-  <el-table></el-table>
+  <el-row :gutter="20">
+    <el-col :span="3" v-for="item in weekData" :key="item.id">
+      <div class="grid-content" :class="item.id === activeIndex ? 'active' : ''">
+        {{ item.label }}
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
-<script setup></script>
+<script setup>
+import { toRefs, reactive } from 'vue'
 
-<style lang="stylus"></style>
+const weekData = [
+  { id: 0, label: '周日' },
+  { id: 1, label: '周一' },
+  { id: 2, label: '周二' },
+  { id: 3, label: '周三' },
+  { id: 4, label: '周四' },
+  { id: 5, label: '周五' },
+  { id: 6, label: '周六' }
+]
+
+const state = reactive({
+  activeIndex: 0
+})
+const getWeek = () => {
+  state.activeIndex = new Date().getDay()
+}
+
+getWeek()
+const { activeIndex } = toRefs(state)
+</script>
+
+<style lang="stylus">
+.grid-content
+  display flex
+  align-items center
+  justify-content center
+  border-radius 4px
+  min-height 36px
+  line-height 36px
+  border 1px solid rgba(255,166,49, .8)
+  cursor pointer
+  &:hover
+    background rgba(255,166,49, .5)
+    color #ffffff
+  &.active
+    background rgba(255,166,49, .8)
+    color #ffffff
+</style>
