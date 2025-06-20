ce4a70d890b133c45ce85142926e9609c72d5587
tanshaobo <18235140339@163.com>
2025-06-20 17:28:13 +0800

    feat(other): 调整圣遗物来源部分信息

---
+++ src/config/Elemental/Anemo/role.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: tanshaobo
+ * @Date: 2025-06-20 17:14:14
+ * @LastEditors: tanshaobo
+ * @LastEditTime: 2025-06-20 17:14:16
+ * @Description: 角色
+ * @FilePath: \yuanshen-utils\src\config\Elemental\Anemo\role.js
+ */
+
