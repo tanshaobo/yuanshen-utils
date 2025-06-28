19a0e9c65d9585c8a86179472ee2fbcccc4988e1
tanshaobo <18235140339@163.com>
2025-06-28 16:24:33 +0800

    feat(other): 调整角色天赋

--- src/config/role.js
+++ src/config/role.js
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2022-08-19 22:45:05
  * @LastEditors: tanshaobo
- * @LastEditTime: 2025-06-28 02:48:53
+ * @LastEditTime: 2025-06-28 16:23:35
  * @Description: 角色配置
  * @FilePath: \yuanshen-utils\src\config\role.js
  */
@@ -323,259 +323,259 @@ export const roleList = [
   {
     id: '0053',
     Name: '柯莱',
-    Talent: 7,
+    Talent: 12,
     header: 'header/柯莱.png'
   },
   {
     id: '0054',
     Name: '提那里',
-    Talent: 7,
+    Talent: 10,
     header: 'header/提那里.png'
   },
   {
     id: '0055',
     Name: '多莉',
-    Talent: 7,
+    Talent: 11,
     header: 'header/多莉.png'
   },
   {
     id: '0056',
     Name: '坎蒂丝',
-    Talent: 7,
+    Talent: 10,
     header: 'header/坎蒂丝.png'
   },
   {
     id: '0057',
     Name: '赛诺',
-    Talent: 7,
+    Talent: 10,
     header: 'header/赛诺.png'
   },
   {
     id: '0058',
     Name: '妮露',
-    Talent: 7,
+    Talent: 12,
     header: 'header/妮露.png'
   },
   {
     id: '0059',
     Name: '纳西妲',
-    Talent: 7,
+    Talent: 11,
     header: 'header/纳西妲.png'
   },
   {
     id: '0060',
     Name: '莱依拉',
-    Talent: 7,
+    Talent: 11,
     header: 'header/莱依拉.png'
   },
   {
     id: '0061',
     Name: '珐露珊',
-    Talent: 7,
+    Talent: 10,
     header: 'header/珐露珊.png'
   },
   {
     id: '0062',
     Name: '流浪者',
-    Talent: 7,
+    Talent: 12,
     header: 'header/流浪者.png'
   },
   {
     id: '0063',
     Name: '瑶瑶',
-    Talent: 7,
+    Talent: 5,
     header: 'header/瑶瑶.png'
   },
-  // {
-  //   id: '0064',
-  //   Name: '艾尔海森',
-  //   Talent: 7,
-  //   header: 'header/艾尔海森.png'
-  // },
-  // {
-  //   id: '0065',
-  //   Name: '迪希雅',
-  //   Talent: 7,
-  //   header: 'header/迪希雅.png'
-  // },
-  // {
-  //   id: '0066',
-  //   Name: '米卡',
-  //   Talent: 7,
-  //   header: 'header/米卡.png'
-  // },
-  // {
-  //   id: '0067',
-  //   Name: '卡维',
-  //   Talent: 7,
-  //   header: 'header/卡维.png'
-  // },
-  // {
-  //   id: '0068',
-  //   Name: '白术',
-  //   Talent: 7,
-  //   header: 'header/白术.png'
-  // },
-  // {
-  //   id: '0069',
-  //   Name: '绮良良',
-  //   Talent: 7,
-  //   header: 'header/绮良良.png'
-  // },
-  // {
-  //   id: '0070',
-  //   Name: '琳妮特',
-  //   Talent: 7,
-  //   header: 'header/琳妮特.png'
-  // },
-  // {
-  //   id: '0071',
-  //   Name: '林尼',
-  //   Talent: 7,
-  //   header: 'header/林尼.png'
-  // },
-  // {
-  //   id: '0072',
-  //   Name: '菲米尼',
-  //   Talent: 7,
-  //   header: 'header/菲米尼.png'
-  // },
-  // {
-  //   id: '0073',
-  //   Name: '纳维莱特',
-  //   Talent: 7,
-  //   header: 'header/纳维莱特.png'
-  // },
-  // {
-  //   id: '0074',
-  //   Name: '莱欧斯利',
-  //   Talent: 7,
-  //   header: 'header/莱欧斯利.png'
-  // },
-  // {
-  //   id: '0075',
-  //   Name: '夏洛蒂',
-  //   Talent: 7,
-  //   header: 'header/夏洛蒂.png'
-  // },
-  // {
-  //   id: '0076',
-  //   Name: '芙宁娜',
-  //   Talent: 7,
-  //   header: 'header/芙宁娜.png'
-  // },
-  // {
-  //   id: '0077',
-  //   Name: '娜维娅',
-  //   Talent: 7,
-  //   header: 'header/娜维娅.png'
-  // },
-  // {
-  //   id: '0078',
-  //   Name: '夏沃蕾',
-  //   Talent: 7,
-  //   header: 'header/夏沃蕾.png'
-  // },
-  // {
-  //   id: '0079',
-  //   Name: '嘉明',
-  //   Talent: 7,
-  //   header: 'header/嘉明.png'
-  // },
-  // {
-  //   id: '0080',
-  //   Name: '闲云',
-  //   Talent: 7,
-  //   header: 'header/闲云.png'
-  // },
-  // {
-  //   id: '0081',
-  //   Name: '千织',
-  //   Talent: 7,
-  //   header: 'header/千织.png'
-  // },
-  // {
-  //   id: '0082',
-  //   Name: '阿蕾奇诺',
-  //   Talent: 7,
-  //   header: 'header/阿蕾奇诺.png'
-  // },
-  // {
-  //   id: '0083',
-  //   Name: '赛索斯',
-  //   Talent: 7,
-  //   header: 'header/赛索斯.png'
-  // },
-  // {
-  //   id: '0084',
-  //   Name: '克洛琳德',
-  //   Talent: 7,
-  //   header: 'header/克洛琳德.png'
-  // },
+  {
+    id: '0064',
+    Name: '艾尔海森',
+    Talent: 11,
+    header: 'header/艾尔海森.png'
+  },
+  {
+    id: '0065',
+    Name: '迪希雅',
+    Talent: 12,
+    header: 'header/迪希雅.png'
+  },
+  {
+    id: '0066',
+    Name: '米卡',
+    Talent: 3,
+    header: 'header/米卡.png'
+  },
+  {
+    id: '0067',
+    Name: '卡维',
+    Talent: 11,
+    header: 'header/卡维.png'
+  },
+  {
+    id: '0068',
+    Name: '白术',
+    Talent: 6,
+    header: 'header/白术.png'
+  },
+  {
+    id: '0069',
+    Name: '绮良良',
+    Talent: 7,
+    header: 'header/绮良良.png'
+  },
+  {
+    id: '0070',
+    Name: '琳妮特',
+    Talent: 15,
+    header: 'header/琳妮特.png'
+  },
+  {
+    id: '0071',
+    Name: '林尼',
+    Talent: 13,
+    header: 'header/林尼.png'
+  },
+  {
+    id: '0072',
+    Name: '菲米尼',
+    Talent: 14,
+    header: 'header/菲米尼.png'
+  },
+  {
+    id: '0073',
+    Name: '纳维莱特',
+    Talent: 13,
+    header: 'header/纳维莱特.png'
+  },
+  {
+    id: '0074',
+    Name: '莱欧斯利',
+    Talent: 15,
+    header: 'header/莱欧斯利.png'
+  },
+  {
+    id: '0075',
+    Name: '夏洛蒂',
+    Talent: 14,
+    header: 'header/夏洛蒂.png'
+  },
+  {
+    id: '0076',
+    Name: '芙宁娜',
+    Talent: 14,
+    header: 'header/芙宁娜.png'
+  },
+  {
+    id: '0077',
+    Name: '娜维娅',
+    Talent: 13,
+    header: 'header/娜维娅.png'
+  },
+  {
+    id: '0078',
+    Name: '夏沃蕾',
+    Talent: 15,
+    header: 'header/夏沃蕾.png'
+  },
+  {
+    id: '0079',
+    Name: '嘉明',
+    Talent: 4,
+    header: 'header/嘉明.png'
+  },
+  {
+    id: '0080',
+    Name: '闲云',
+    Talent: 6,
+    header: 'header/闲云.png'
+  },
+  {
+    id: '0081',
+    Name: '千织',
+    Talent: 9,
+    header: 'header/千织.png'
+  },
+  {
+    id: '0082',
+    Name: '阿蕾奇诺',
+    Talent: 15,
+    header: 'header/阿蕾奇诺.png'
+  },
+  {
+    id: '0083',
+    Name: '赛索斯',
+    Talent: 12,
+    header: 'header/赛索斯.png'
+  },
+  {
+    id: '0084',
+    Name: '克洛琳德',
+    Talent: 14,
+    header: 'header/克洛琳德.png'
+  },
   {
     id: '0085',
     Name: '希格雯',
-    Talent: 7,
+    Talent: 13,
     header: 'header/希格雯.png'
   },
   {
     id: '0086',
     Name: '艾梅莉埃',
-    Talent: 7,
+    Talent: 15,
     header: 'header/艾梅莉埃.png'
   },
   {
     id: '0087',
     Name: '卡齐娜',
-    Talent: 7,
+    Talent: 18,
     header: 'header/卡齐娜.png'
   },
   {
     id: '0088',
     Name: '玛拉妮',
-    Talent: 7,
+    Talent: 16,
     header: 'header/玛拉妮.png'
   },
   {
     id: '0089',
     Name: '基尼奇',
-    Talent: 7,
+    Talent: 17,
     header: 'header/基尼奇.png'
   },
   {
     id: '0090',
     Name: '希诺宁',
-    Talent: 7,
+    Talent: 17,
     header: 'header/希诺宁.png'
   },
   {
     id: '0091',
     Name: '欧若伦',
-    Talent: 7,
+    Talent: 17,
     header: 'header/欧若伦.png'
   },
   {
     id: '0092',
     Name: '恰斯卡',
-    Talent: 7,
+    Talent: 18,
     header: 'header/恰斯卡.png'
   },
   {
     id: '0093',
     Name: '茜特菈莉',
-    Talent: 7,
+    Talent: 17,
     header: 'header/茜特菈莉.png'
   },
   {
     id: '0094',
     Name: '玛薇卡',
-    Talent: 7,
+    Talent: 16,
     header: 'header/玛薇卡.png'
   },
   {
     id: '0095',
     Name: '蓝砚',
-    Talent: 7,
+    Talent: 5,
     header: 'header/蓝砚.png'
   },
   {
@@ -587,37 +587,37 @@ export const roleList = [
   {
     id: '0097',
     Name: '伊安珊',
-    Talent: 7,
+    Talent: 16,
     header: 'header/伊安珊.png'
   },
   {
     id: '0098',
     Name: '瓦雷莎',
-    Talent: 7,
+    Talent: 18,
     header: 'header/瓦雷莎.png'
   },
   {
     id: '0099',
     Name: '伊法',
-    Talent: 7,
+    Talent: 18,
     header: 'header/伊法.png'
   },
   {
     id: '0100',
     Name: '爱可菲',
-    Talent: 7,
+    Talent: 14,
     header: 'header/爱可菲.png'
   },
   {
     id: '0101',
     Name: '塔利雅',
-    Talent: 7,
+    Talent: 3,
     header: 'header/塔利雅.png'
   },
   {
     id: '0102',
     Name: '丝柯克',
-    Talent: 7,
+    Talent: 16,
     header: 'header/丝柯克.png'
   },
   {
